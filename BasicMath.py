b52ea237cb18fbb437dd9493cd56297e53761f46
07VishnuHub <07vishnuatwork@gmail.com>
2025-05-23 10:15:14 +0530

    Create BasicMath.py

---
+++ BasicMath.py
@@ -0,0 +1,9 @@
+#Task 1: Perform Basic Mathematical Operations
+
+number1=input("Enter first number: ")
+number2=input("Enter second number: ")
+
+print("Addition: ", int(number1) + int(number2))
+print("Subtraction: ", int(number1) - int(number2))
+print("Multiplication: ", int(number1) * int(number2))
+print("Division: ", int(number1) / int(number2))
