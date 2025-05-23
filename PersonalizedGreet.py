3ccce19c1ef274e68440e8969a9401ce2541520a
07VishnuHub <07vishnuatwork@gmail.com>
2025-05-23 10:18:08 +0530

    Create PersonalizedGreet.py

---
+++ PersonalizedGreet.py
@@ -0,0 +1,7 @@
+#Task 2: Create a Personalized Greeting
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Create a personalized greeting
+print("Hello, " + first_name + " " + last_name + "! Welcome to the Python programming world.")
